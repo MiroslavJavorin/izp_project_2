eb601c28ba25957aef14274dfeb5e066be9cb1b5
MiroslavJavorin <skuratovich.aliaksandr@gmail.com>
2020-11-23 05:22:08 +0100

    working on function get_file()

--- sps.c
+++ sps.c
@@ -1,12 +1,25 @@
+/**
+ * Project 2 - simple spreadsheet editor 2
+ * Subject IZP 2020/21
+ * @Author Skuratovich Aliaksandr xskura01@fit.vutbr.cz
+*/
+
+//region includes
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
+//endregion
 
 //region macros
+/*
+ *  MEMBUG  - debug memory usage
+ *  SEPSBUG - debug separators
+ * */
+
 /* checks if exit code is greater than 0, which means error has been occurred,
  * calls function that prints an error on the stderr and returns an error */
-#define CHECK_EXIT_CODE_RUN_PROGRAM_FUNCTION if(exit_code > 0)\
+#define CHECK_EXIT_CODE_IN_RUN_PROGRAM if(exit_code > 0)\
 {\
     print_error_message(&exit_code);\
     return exit_code;\
@@ -20,29 +33,33 @@ enum erorrs
 {
     ALLOCATING_ERROR = 1,
     UNSUPPORTED_SEPARATORS_ERROR,
-    TOO_FEW_ARGS_ERROR
+    TOO_FEW_ARGS_ERROR,
+    NO_SUCH_FILE_ERROR
 };
+
 //endregion
 
 /* contains information about delim string */ // TODO generalize
-typedef struct seps_t
+typedef struct carr_t
 {
-    int seps_c;    /* number of separators user entered in delim string in commandline */
-    char *seps_v;   /* dynamically allocated array of chars that contains separators */
-} seps_t;
+    int   elems_c;    /* number of separators user entered in delim string in commandline */
+    char *elems_v;   /* dynamically allocated array of chars that contains separators */
+} carr_t;
 
 //region structures
 /* contains information about arguments user entered in commandline */
 typedef struct
 {
-    seps_t seps;
+    carr_t seps;
+    FILE  *ptr;
 } clargs_t;
 
 /* contains information about a table */
 typedef struct
 {
-    int param1;
-    int param2;
+    carr_t***     table; /* table contains rows which contain cells */
+    unsigned int  row_c; /* number of rows of the table */
+    unsigned int* col_c; /* number of columns of each row */
 } table_t;
 //endregion
 
@@ -55,7 +72,16 @@ void free_table(table_t *s)
 /* frees all memory allocated by a clargs_t structure s */
 void free_clargs(clargs_t *s)
 {
-    free(s->seps.seps_v);
+    if(s->seps.elems_v)
+        free(s->seps.elems_v);
+    if(s->ptr)
+        fclose(s->ptr);
+}
+
+void clear_data(table_t *table, clargs_t *clargs)
+{
+    free_table(table);
+    free_clargs(clargs);
 }
 
 /**
@@ -70,31 +96,21 @@ void free_clargs(clargs_t *s)
  *         true, if item has been added to an array
  *         false, if item is already in the array or if error while allocating memory has been occurred
  */
-bool set_add_item(seps_t *set, char item, int *exit_code)
+bool set_add_item(carr_t *set, char item, int *exit_code)
 {
-#ifdef SEPBUG
-    printf("line %d in %s -> size     =%d\n", __LINE__, __FUNCTION__, set->seps_c);
-#endif
-    for(int i = 0; i < set->seps_c; i++)
-    {
-        printf("i =%d, size =%d, set[i] =%d\n", i , set->seps_c, set->seps_v[i]);
-        if(set->seps_v[i] == item)
+    for(int i = 0; i < set->elems_c; i++)
+        if(set->elems_v[i] == item)
             return false;
-    }
-#ifdef MEMBUG
-    printf("line %d in %s -> size bef =%d\n",__LINE__, __FUNCTION__, set->seps_c);
-#endif
 
-    set->seps_v = (char *)realloc(set->seps_v, (unsigned long)++set->seps_c * sizeof(char));
-    if(set->seps_v == NULL)
+
+    set->elems_v = (char *)realloc(set->elems_v, (unsigned long)++set->elems_c * sizeof(char));
+    if(set->elems_v == NULL)
     {
         *exit_code = ALLOCATING_ERROR;
         return false;
     }
-#ifdef MEMBUG
-    printf("line %d in %s -> size aft =%d\n",__LINE__, __FUNCTION__, set->seps_c);
-#endif
-    set->seps_v[set->seps_c-1] = item;
+
+    set->elems_v[set->elems_c-1] = item;
     return true;
 }
 
@@ -105,29 +121,28 @@ bool set_add_item(seps_t *set, char item, int *exit_code)
  * @param argv An array with commandline arguments
  * @param exit_code exit code to change if an error occurred
  */
-void init_separators(int argc, char **argv, clargs_t *clargs, int *exit_code)
+void init_separators(const int argc, const char **argv, clargs_t *clargs, int *exit_code)
 {
     //region variables
-    clargs->seps.seps_c = 0;
-    clargs->seps.seps_v = NULL;
-    char* sepsptr = NULL;
+    clargs->seps.elems_c = 0;
+    clargs->seps.elems_v = NULL;
     int k = 0;
     //endregion
 
     /* if there is only name, functions and filename */
-    if(argc > 3 && strcmp(argv[1], "-d"))
+    if(argc >= 3 && strcmp(argv[1], "-d"))
     {
         set_add_item(&(clargs->seps), ' ', exit_code);
         CHECK_EXIT_CODE
     }
 
     /* if user entered -d flag and there are function names and filename in the commandline */
-    else if(argc > 4 && !strcmp(argv[1], "-d"))
+    else if(argc >= 5 && !strcmp(argv[1], "-d"))
     {
         while(argv[2][k])
         {
             /* checks if the user has not entered characters that will lead to undefined program behavior */
-            if(argv[2][k] == 10 || argv[2][k] == 92 || argv[2][k] == 39 || argv[2][k] == 34)
+            if(argv[2][k] == 10 || argv[2][k] == 92 || argv[2][k] == 34)
             {
                 *exit_code = UNSUPPORTED_SEPARATORS_ERROR;
                 return;
@@ -145,13 +160,27 @@ void init_separators(int argc, char **argv, clargs_t *clargs, int *exit_code)
         *exit_code = TOO_FEW_ARGS_ERROR;
         return;
     }
-    clargs->seps.seps_v = sepsptr;
+#ifdef SEPSBUG
+    printf("line %d in %s separators -> ", __LINE__, __FUNCTION__);
+    for(int i = 0; i < clargs->seps.elems_c; i++ )
+        printf("%c",clargs->seps.elems_v[i]);
+    putchar(10);
+#endif
 }
 
-void clargs_init(int argc, char **argv, clargs_t* clargs, int *exit_code)
+void get_table(const int argc, const char *argv, table_t *table, clargs_t *clargs, int *exit_code)
 {
-    init_separators(argc, argv, clargs, exit_code);
-    CHECK_EXIT_CODE
+    if((clargs->ptr = fopen(argv[argc - 1], "r+")) == NULL)
+    {
+        *exit_code = NO_SUCH_FILE_ERROR;
+        return;
+    }
+
+    /* if file has been opened successfully */
+    else
+    {
+
+    }
 }
 
 void process_table(clargs_t *clargs, table_t *table, int *exit_code)
@@ -168,11 +197,12 @@ void print_error_message(int *exit_code)
     {
             "Twenty afternoons in utopia",
             "Entered separators are not supported buy the program",
-            "You've entered too few arguments"
+            "You've entered too few arguments",
+            "There is no file with this name"
     };
 
     /* print an error message to stderr */
-    fprintf(stderr, "Error %d: %s\n", *exit_code, error_msg[*exit_code - 1]);
+    fprintf(stderr, "Error %d: %s.\n", *exit_code, error_msg[*exit_code - 1]);
 }
 
 /**
@@ -180,7 +210,7 @@ void print_error_message(int *exit_code)
  * After every initializing checks for an exit_code to return it if the error has been occurred
  * Frees memory that was allocated while program was running.
  */
-int run_program(int argc, char **argv)
+int run_program(const int argc, const char **argv)
 {
     //region arguments
     int exit_code = 0;
@@ -188,16 +218,16 @@ int run_program(int argc, char **argv)
     clargs_t clargs;
     //endregion
 
-    /* initialize all commandline arguments and add them to the structure to use when processing table */
-    clargs_init(argc, argv, &clargs, &exit_code);
-    CHECK_EXIT_CODE_RUN_PROGRAM_FUNCTION
+    /* initialize separators from commandline */
+    init_separators(argc, argv, &clargs, &exit_code);
+    CHECK_EXIT_CODE_IN_RUN_PROGRAM
 
-    /* process the table with the given cl arguments */
-    process_table(&clargs, &table, &exit_code);
-    CHECK_EXIT_CODE_RUN_PROGRAM_FUNCTION
+    /* adds table to the structure */
+    get_table(argc, argv, &table, &clargs, &exit_code);
+    CHECK_EXIT_CODE_IN_RUN_PROGRAM
 
-    free_table(&table);
-    free_clargs(&clargs);
+    clear_data(&table, &clargs);
+
     /* exit_code is -1 means table has been processed successfully */
     return (exit_code == -1) ? 0 : exit_code;
 }
@@ -208,7 +238,7 @@ void print_documentation()
     printf("Kiss an ugly turtle to make it cry");
 }
 
-int main(int argc, char **argv)
+int main(const int argc, const char **argv)
 {
     /* print documentation */
     if(argc == 2 && (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")))
